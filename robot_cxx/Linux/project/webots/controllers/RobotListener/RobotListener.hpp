ad722e9d553eeeca1ce783fbfda4cc145b0578c9
Rolo Choo <faxaistudy@gmail.com>
2025-11-27 17:00:49 +0900

    Update RobotListener.hpp

--- robot_cxx/Linux/project/webots/controllers/RobotListener/RobotListener.hpp
+++ robot_cxx/Linux/project/webots/controllers/RobotListener/RobotListener.hpp
@@ -1,19 +1,19 @@
-// File:          Walk.hpp
-// Date:          January 2013
-// Description:   Example showing how to use the gait manager
-//                and keyboard inputs
-// Author:        david.mansolino@epfl.ch
+// File:          RobotListener.hpp
+// Description:   DARwIn-OP Integrated Controller Header
+//                (Walk + Motion + Camera + HTTP Server)
 
-#ifndef WALK_HPP
-#define WALK_HPP
+#ifndef ROBOT_LISTENER_HPP
+#define ROBOT_LISTENER_HPP
 
 #define NMOTORS 20
 
 #include <webots/Robot.hpp>
 
+// 전방 선언 (Forward Declarations)
 namespace managers {
   class DARwInOPMotionManager;
   class DARwInOPGaitManager;
+  class DARwInOPVisionManager; // 비전 매니저 추가
 }
 
 namespace webots {
@@ -22,27 +22,33 @@ namespace webots {
   class Camera;
   class Accelerometer;
   class Gyro;
-  class Speaker;
+  // Speaker는 현재 안 쓰므로 제거하거나 남겨둬도 무방
 };
 
-class Walk : public webots::Robot {
+class RobotListener : public webots::Robot {
   public:
-                                     Walk();
-    virtual                         ~Walk();
+                                     RobotListener();
+    virtual                         ~RobotListener();
     void                             run();
-    void                             checkIfFallen();
-    
+
   private:
     int                              mTimeStep;
     
     void                             myStep();
     void                             wait(int ms);
-    
+    void                             checkIfFallen(); // private으로 이동 (내부 로직용)
+
+    // Devices
     webots::Motor                   *mMotors[NMOTORS];
+    webots::LED                     *mEyeLED;
+    webots::LED                     *mHeadLED;
+    webots::Camera                  *mCamera;
     webots::Accelerometer           *mAccelerometer;
     
+    // Managers
     managers::DARwInOPMotionManager *mMotionManager;
     managers::DARwInOPGaitManager   *mGaitManager;
+    managers::DARwInOPVisionManager *mVisionManager; // Vision Manager 포인터 추가
 };
 
 #endif
